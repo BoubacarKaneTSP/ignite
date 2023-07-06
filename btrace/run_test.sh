f7bde284e7d12987ae1552193e3e41c66685151a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-07-06 23:12:59 +0200

    testing perf without long adder

--- btrace/run_test.sh
+++ btrace/run_test.sh
@@ -15,10 +15,10 @@ operationcount=10000000
 recordcount=1000000
 output_file_load="outputload.txt"
 output_file_run="outputrun.txt"
-load_type="LOAD"
-#load_type="LOAD_no_LongAdder"
-run_type="RUN"
-#run_type="RUN_no_Long_adder"
+#load_type="LOAD"
+load_type="LOAD_no_LongAdder"
+#run_type="RUN"
+run_type="RUN_no_Long_adder"
 
 flag_append="w"
 
