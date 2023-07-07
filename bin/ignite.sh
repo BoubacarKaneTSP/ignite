8b4bd6bfca68db8b60b0f24cee69d1ffb94cbcb2
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-07-07 13:35:32 +0200

    testing perf without long adder

--- bin/ignite.sh
+++ bin/ignite.sh
@@ -93,10 +93,10 @@ fi
 #
 
 #JVM_OPTS= btracer -o outputBtrace.txt ../btrace/Trace.class
-if [ -z "$JVM_OPTS" ] ; then
-    JVM_OPTS="-Xms1g -Xmx1g -server -XX:MaxMetaspaceSize=256m"
+if [ -z "$JVM_OPTS" ] || [ "$JVM_OPTS" == "-Dfile.encoding=UTF-8" ] ; then
+    JVM_OPTS="btracer -o outputBtrace.txt ../btrace/Trace.class -Xms1g -Xmx1g -server -XX:MaxMetaspaceSize=256m"
 else
-  echo "$JVM_OPTS"
+  echo "No JVM options"
 fi
 
 #
