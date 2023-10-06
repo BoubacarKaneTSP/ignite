d7be0d0a5970f809d7e874bb37d5b06454c9fc4d
Boubacar Kane <kaneb@bran>
2023-10-06 23:50:05 +0200

    update

--- btrace/run_test.sh
+++ btrace/run_test.sh
@@ -4,7 +4,7 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=10
+nbTest=30
 #clients="1 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200"
 #clients="90 110 130 150 170 190 200"
 clients="190"
@@ -12,8 +12,8 @@ clients="190"
 list_stat="OVERALL-RunTime OVERALL-Throughput"
 hosts="127.0.0.1"
 #workload="workloada"
-workloads="workloada" #workloadb workloadc workloadf workloadd"
-operationcount=60000000
+workloads="workloadf" #workloadb workloadc workloadf workloadd"
+operationcount=100000000
 recordcount=1000000
 output_file_load="outputload.txt"
 output_file_run="outputrun.txt"
@@ -21,7 +21,6 @@ load_type="LOAD"
 #load_type="LOAD_LongAdder"
 run_type="RUN"
 #run_type="RUN_LongAdder"
-
 flag_append="w"
 
 cd $YCSB_HOME || exit
